d4c3519cbbe05e622c95edb0ad05748011a330a0
Kasai10 <simobeneckert@gmail.com>
2025-07-17 21:33:23 +0200

    enabled production view

--- app_streamlit.py
+++ app_streamlit.py
@@ -283,4 +283,4 @@ def update_timer(n_intervals, input_time, timer_data, start_timestamp):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)
